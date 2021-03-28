a711c00c6d034cf161d268dcb29acd1413d1492e
unknown <habiibaz1404@gmail.com>
2021-03-29 06:14:35 +0700

    add templates and static files

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='libre-home'),
-    path('register/', views.register, name='libre-register'),
-    path('login/', views.login, name='libre-login'),
+    path('', views.landingpage, name='libre-landingpage'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-def home(request):
-	return render(request, 'blog/home.html', {'title': 'Home'})
-
-def register(request):
-	return render(request, 'blog/register.html', {'title': 'Register'})
-
-def login(request):
-	return render(request, 'blog/login.html', {'title': 'Login'})
+def landingpage(request):
+	return render(request, 'blog/landingpage.html', {'title': 'Landing Page'})
